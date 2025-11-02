667bbff0a89b4b4c087955394462228bfcce1b32
Froggy-IT <kalzhanovzhansultan@gmail.com>
2025-11-02 23:44:42 +0500

    sorry the first didn't work properly

--- src/test/java/org/smartcity/GraphTest.java
+++ src/test/java/org/smartcity/GraphTest.java
@@ -1,9 +1,12 @@
 import graph.util.Graph;
+import graph.scc.TarjanSCC;
+import graph.scc.SCCResult;
+import graph.topo.KahnTopologicalSort;
+import graph.dagsp.DAGShortestLongest;
+import graph.dagsp.PathResult;
+
 import org.junit.jupiter.api.Test;
-import graph.scc.*;
-import graph.topo.*;
-import graph.dagsp.*;
-import java.util.*;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,41 +14,54 @@ public class GraphTest {
 
     @Test
     void testSmallGraph() {
-        Graph g = new Graph(3, true);
+        Graph g = new Graph(3);
         g.addEdge(0, 1, 1);
         g.addEdge(1, 2, 2);
         assertEquals(3, g.size());
     }
-
     @Test
     void testTarjanSCC() {
-        Graph g = new Graph(3, true);
+        Graph g = new Graph(2);
         g.addEdge(0, 1, 1);
         g.addEdge(1, 0, 1);
-        TarjanSCC t = new TarjanSCC();
-        var sccs = t.findSCCs(g);
-        assertEquals(1, sccs.size());
+        SCCResult res = TarjanSCC.run(g, null);
+
+        List<List<Integer>> comps = res.components;
+
+        assertEquals(1, comps.size(), "Expected 1 SCC for 0↔1 cycle");
+        assertTrue(comps.get(0).containsAll(List.of(0, 1)));
     }
 
     @Test
     void testTopologicalSort() {
-        Graph g = new Graph(3, true);
+        Graph g = new Graph(3);
         g.addEdge(0, 1, 1);
         g.addEdge(1, 2, 1);
-        KahnSort k = new KahnSort();
-        var order = k.topologicalSort(g);
+        var order = KahnTopologicalSort.sort(g, null);
         assertEquals(List.of(0, 1, 2), order);
     }
 
     @Test
     void testDAGShortestPath() {
-        Graph g = new Graph(3, true);
+        Graph g = new Graph(3);
         g.addEdge(0, 1, 1);
         g.addEdge(1, 2, 2);
-        DAGShortestPath sp = new DAGShortestPath();
-        var dist = sp.shortestPath(g, 0);
-        assertEquals(0, dist.get(0));
-        assertEquals(1, dist.get(1));
-        assertEquals(3, dist.get(2));
+        PathResult res = DAGShortestLongest.shortest(g, 0, null);
+
+        assertEquals(0, res.dist[0]);
+        assertEquals(1, res.dist[1]);
+        assertEquals(3, res.dist[2]);
+    }
+
+    @Test
+    void testDAGLongestPath() {
+        Graph g = new Graph(3);
+        g.addEdge(0, 1, 2);
+        g.addEdge(1, 2, 3);
+        PathResult res = DAGShortestLongest.longest(g, 0, null);
+
+        assertEquals(0, res.dist[0]);
+        assertEquals(2, res.dist[1]);
+        assertEquals(5, res.dist[2]);
     }
 }
